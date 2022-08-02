e36b2733780e1471f990a8abd821f0d9a3b60c42
Sharry <sharrysar@users.noreply.github.com>
2022-08-02 15:59:28 +0000

    First mixed list

---
+++ mix-list/mixlist01.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+# list of IP addresses, ports, and protocol
+iplist = [ 5060, "80", 55, "10.0.0.1", "10.20.30.1", "ssh" ]
+
+# display only the IP addresses to the screen
+print("IP addresses: ", iplist[3], " and", iplist[4])
+

89feebd85e1b8092aef4e7019028ac13fef103b3
Sharry <sharrysar@users.noreply.github.com>
2022-08-03 16:45:05 +0000

    Modified to return value as input

--- iftest/condition03.py
+++ iftest/condition03.py
@@ -3,7 +3,7 @@
 hostname = input("What value should we set for hostname?")
 
 if hostname.lower() == "mtg":
-    print("The hostname was found to be mtg")
+    print(f"The hostname was found to be {hostname}")
     print("hostname matches the expected config")
    
 # printing out to user   

1b14ec9c14d6fa9d1c95ec117c7720b371fe0dda
sharry <sharrysar@users.noreply.github.com>
2022-08-10 17:36:39 +0000

    created a class

--- oop-intro/creatingclass.py
+++ oop-intro/creatingclass.py
@@ -1,19 +1,43 @@
-#!/usr/bin/env python3
-import random
+#!/usr/bin/python3
+"""RZFeeser | Alta3 Research
+   Creating a simple dice program utilizing classes."""
 
-player1_dice = []
-player2_dice = []
+# standard library
+from random import randint
 
-for i in range(3):
-  player1_dice.append(random.randint(1,6))
-  player2_dice.append(random.randint(1,6))
+class Player:
+    def __init__(self):
+        self.dice = []
 
-print("Player 1 rolled" + str(player1_dice))
-print("Player 2 rolled" + str(player2_dice))
+    def roll(self):
+        self.dice = [] # clears current dice
+        for i in range(3):  # make 3 rolls
+            self.dice.append(randint(1,6))   # 1 to 6 inclusive
 
-if sum(player1_dice) == sum(player2_dice):
-  print("Draw")
-elif sum(player1_dice) > sum(player2_dice):
-  print("Player 1 wins")
-else:
-  print("Player 2 wins")
+    def get_dice(self): # returns the dice rolls
+        return self.dice
+
+def main():
+    """called at run time"""
+
+    ## create our player objects
+    player1 = Player()
+    player2 = Player()
+
+    player1.roll()
+    player2.roll()
+
+    print(f"Player 1 rolled {player1.get_dice()}")
+    print(f"Player 2 rolled {player2.get_dice()}")
+
+    # determine which player won
+    if sum(player1.get_dice()) == sum(player2.get_dice()):
+        print("Draw!")
+    elif sum(player1.get_dice()) > sum(player2.get_dice()):
+        print("Player 1 wins!")
+    else:
+        print("Player 2 wins!")
+
+
+if __name__ == "__main__":
+    main()
